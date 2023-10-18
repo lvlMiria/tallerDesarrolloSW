0fc3c09db67e848c9c357216233fb1dcd8954f24
Miria <127509085+lvlMiria@users.noreply.github.com>
2023-10-18 11:47:35 -0300

    Add files via upload
    Enum

---
+++ ProyectoDosCapas/Logica/IContactoBLL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public interface IContactoBLL
+    {
+        void Agregar(Persona per);
+        List<Persona> Listar();
+        List<Persona> Listar(Clasificacion grupo);
+    }
+}
---
+++ ProyectoDosCapas/Presentacion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logica;
+
+namespace Presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactoBLL cbll = new ContactoBLL();
+            Persona persona1 = new Persona("Helena","Klein",12345678);
+            Persona persona2 = new Persona("Juan Carlos", "Bodoque", 87654321);
+            Persona persona3 = new Persona("Lucas", "Perez", 23456789);
+
+            persona1.Grupo = Clasificacion.Familia;
+
+            cbll.Agregar(persona1);
+            cbll.Agregar(persona2);
+            cbll.Agregar(persona3);
+
+            Console.WriteLine("Todos los contactos");
+            foreach (Persona per in cbll.Listar())
+            {
+                Console.WriteLine(per);
+            }
+
+            Console.WriteLine("\nGrupo Familia");
+            foreach (Persona per in cbll.Listar(Clasificacion.Familia))
+            {
+                Console.WriteLine(per);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
